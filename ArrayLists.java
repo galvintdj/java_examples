e23ff1c75caf0e278cab76d087dcb36a198f5d63
Tim Galvin <galvintdj@gmail.com>
2017-10-20 21:53:36 -0400

    Updated ArrayLists.java to create an array list, add, remove, sort, and print it out.

--- ArrayLists.java
+++ ArrayLists.java
@@ -5,17 +5,35 @@ public class ArrayLists{
 	
 	public static void main (String[] args) {
 
-		ArrayList<String> languages = new ArrayList<String>(3);
+		ArrayList<String> languages = new ArrayList<String>();
 		
 		languages.add("Spanish");
 		languages.add("French");
 		languages.add("English");
+		languages.add("Polish");
+		languages.add("Java");
 	
 		System.out.println(languages.size());
 	
 		languages.add("German");
 
 		System.out.println(languages.size());
-		System.out.println(languages.get(1));
+		System.out.println("getting rid of French");
+		languages.remove("French");
+		System.out.println(languages.get(3));
+
+		//for (int i=0 ; i < languages.size(); i++) {
+		System.out.println(" --- Begin Unsorted List ---");
+		System.out.println( languages );
+		System.out.println(" --- End Unsorted List ---");
+
+		Collections.sort(languages);
+
+		System.out.println(" --- Begin Sorted List ---");
+		System.out.println( languages );
+		System.out.println(" --- End Sorted List ---");
+
+		//}
+
 	}
 }
